9197a403e2cf76a246ed94f60fb20ce48da62424
Aleksandr <soft@vhlv.ru>
2022-01-22 17:32:02 +0300

    feat: add functional burger menu

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,6 +1,25 @@
+
+const menu = document.querySelector('.header__menu');
 const menuButton = document.querySelector('.header__menu-button');
+
+
 function toggleMenuButton() {
+  menu.classList.toggle('header__menu_opened');
   menuButton.classList.toggle('header__menu-button_opened');
+  menuButton.getAttribute('aria-label') === 'Открыть меню'
+    ? menuButton.setAttribute('aria-label', 'Закрыть меню')
+    : menuButton.setAttribute('aria-label', 'Открыть меню');
+}
+
+function closeMenu(e) {
+  if (e.target.classList.contains('header__menu-link')) {
+    menuButton.classList.remove('header__menu-button_opened');
+    menu.classList.remove('header__menu_opened');
+    menuButton.setAttribute('aria-label', 'Открыть меню')
+  }
+
 }
 
 menuButton.addEventListener('click', toggleMenuButton);
+
+menu.addEventListener('click', closeMenu);
