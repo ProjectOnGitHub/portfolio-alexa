0ab1b0d9fedf3ca6a554ff5938d1b1350f41d7b1
Aleksandr <soft@vhlv.ru>
2022-01-26 23:23:14 +0300

    feat: add light theme for mobile

--- portfolio/index.js
+++ portfolio/index.js
@@ -3,9 +3,14 @@ const menu = document.querySelector('.header__menu');
 const menuButton = document.querySelector('.header__menu-button');
 const themeButton = document.querySelector('.header__theme-button');
 const activePortfolioButton = document.querySelector('.portfolio__filter-button_active');
+const pageWidth = document.documentElement.scrollWidth;
 
-const lightThemeMainSelectors = ['.skills', '.portfolio', '.video', '.price', '.skills__list', '.portfolio__filter-button', '.section__title-cover', '.header__menu_opened', '.price__subtitle', '.price__sublist'];
+const lightThemeMainSelectors = ['.skills', '.portfolio', '.video', '.price', '.skills__list', '.section__title-cover', '.price__subtitle', '.price__sublist'];
 const lightThemeTitleSelectors = ['.section__title'];
+const lightThemeMenuSelectors = ['.header__menu', '.header__menu-link'];
+const lightThemePortfolioSelectors = ['.portfolio__filter-button'];
+
+
 
 const getSelectors = (arr, selector) => {
   arr.map((item) => Array
@@ -16,50 +21,45 @@ const getSelectors = (arr, selector) => {
 
 const toggleTheme = () => {
   getSelectors(lightThemeMainSelectors, 'light-theme');
-  getSelectors(lightThemeTitleSelectors, 'light-theme_section-title');
-  activePortfolioButton.classList.remove('light-theme');
-
+  getSelectors(lightThemeTitleSelectors, 'light-theme__section-title');
+  getSelectors(lightThemeMenuSelectors, 'light-theme__burger-menu');
+  getSelectors(lightThemePortfolioSelectors, 'light-theme__portfolio-button');
+  activePortfolioButton.classList.remove('light-theme__portfolio-button');
+  activePortfolioButton.classList.toggle('light-theme__portfolio-button_active');
   themeButton.classList.toggle('header__theme-button_dark');
+
   themeButton.getAttribute('aria-label') === 'Переключить на светлую тему'
     ? themeButton.setAttribute('aria-label', 'Переключить на тёмную тему')
     : themeButton.setAttribute('aria-label', 'Переключить на светлую тему');
 }
 
-
-
-
 function toggleMenuButton() {
   menu.classList.toggle('header__menu_opened');
-  menuButton.classList.toggle('header__menu-button_opened');
   menuButton.getAttribute('aria-label') === 'Открыть меню'
     ? menuButton.setAttribute('aria-label', 'Закрыть меню')
     : menuButton.setAttribute('aria-label', 'Открыть меню');
+  menu.classList.contains('light-theme__burger-menu')
+    ? menuButton.classList.toggle('light-theme__burger-menu-button')
+    : menuButton.classList.toggle('header__menu-button_opened');
 
 }
 
+
+
+
+
 function closeMenu(e) {
   if (e.target.classList.contains('header__menu-link')) {
+    menuButton.classList.remove('light-theme__burger-menu-button');
     menuButton.classList.remove('header__menu-button_opened');
     menu.classList.remove('header__menu_opened');
     menuButton.setAttribute('aria-label', 'Открыть меню')
   }
-
 }
 
 menuButton.addEventListener('click', toggleMenuButton);
 
 menu.addEventListener('click', closeMenu);
+
 themeButton.addEventListener('click', toggleTheme);
 
-
-
-console.log(`
-Ваша отметка - 83 балла(ов)
-Отзыв по пунктам ТЗ:
-Не выполненные/не засчитанные пункты:
-1) бургер-иконка, которая при клике превращается в крестик, создана при помощи css-анимаций без использования изображений
-
-
-Все оставшиеся пункты выполнены и не имеют комментариев проверяющего.
-
-`);
